46686de2387f560f9714b81d692870462b391830
rohit <rohitkedar2000@gmail.com>
2023-11-08 18:50:23 +0530

    added delete functionality

--- src/TodoContext.jsx
+++ src/TodoContext.jsx
@@ -14,11 +14,18 @@ export function TodoProvider({ children }) {
   const addTodo = (newTodo) => {
     if (newTodo.trim() !== '') {
       setTodoList([...todoList, newTodo]);
+      setTodo('');
     }
   };
 
+  const removeTodo = (index) => {
+    const updatedTodoList = [...todoList];
+    updatedTodoList.splice(index, 1);
+    setTodoList(updatedTodoList);
+  };
+
   return (
-    <TodoContext.Provider value={{ todo, setTodo, todoList, addTodo }}>
+    <TodoContext.Provider value={{ todo, setTodo, todoList, addTodo,removeTodo }}>
       {children}
     </TodoContext.Provider>
   );
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,14 +2,18 @@ import { useTodoContext } from "../TodoContext";
 
 
 function TaskList() {
-  const { todoList } = useTodoContext();
+  const { todoList,removeTodo } = useTodoContext();
 
   return (
-    <ul>
+    <ol>
       {todoList.map((item, index) => (
-        <li key={index}>{item}</li>
+        <li key={index}>
+          {item}
+       
+          <button onClick={() => removeTodo(index)}>x</button>
+        </li>
       ))}
-    </ul>
+    </ol>
   );
 }
 
