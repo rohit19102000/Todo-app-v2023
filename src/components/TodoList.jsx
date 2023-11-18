88fc910ea7feec174d3b4b4e3765d1d398cc9378
rohit <rohitkedar2000@gmail.com>
2023-11-18 15:13:08 +0530

    Add category functionalityb | v2

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,24 +1,32 @@
 import { useTodoContext } from '../TodoContext';
 
 function TodoList() {
-  const { todoList, removeTodo,toggleStatus,editTodo } = useTodoContext();
+  const { todoList, removeTodo, toggleStatus, editTodo } = useTodoContext();
+  const { todo } = useTodoContext();
+
+  const filteredTodoList = todoList.filter((item) => {
+    if (todo.category === 'All') {
+      return true; 
+    } else {
+      return item.status === (todo.category === 'Done');
+    }
+  });
 
   return (
     <div className='todoList'>
-      {todoList.map((item, index) => (
-        <div key={index}  className='todoCard'>
-          <strong >{item.text} </strong> 
-          <p style={{fontSize:".5rem"} }>{item.status=== true ? "done" : "pending"}</p>
-          <div className=' details'>
-
-          <p>{item.time}</p>
-          <p>{item.date}</p>  
-
-          <h6></h6>
-          <button style={{fontSize:"1.5rem",padding:"5px"}} onClick={() => editTodo(index)}>✍</button>
-
-          <button onClick={()=> toggleStatus(index)}>{item.status ? "Done" : "pending"}</button>
-          <button onClick={() => removeTodo(index)}>🗑</button>
+      {filteredTodoList.map((item) => (
+        <div key={item.id} className='todoCard'>
+          <strong>{item.text}</strong>
+          <p style={{ fontSize: '.5rem' }}>{item.status === true ? 'done' : 'pending'}</p>
+          <div className='details'>
+            <p>{item.time}</p>
+            <p>{item.date}</p>
+            <h6></h6>
+            <button style={{ fontSize: '1.5rem', padding: '5px' }} onClick={() => editTodo(item.id)}>
+              ✍
+            </button>
+            <button onClick={() => toggleStatus(item.id)}>{item.status ? 'Done' : 'Pending'}</button>
+            <button onClick={() => removeTodo(item.id)}>🗑</button>
           </div>
         </div>
       ))}
@@ -27,4 +35,3 @@ function TodoList() {
 }
 
 export default TodoList;
-
