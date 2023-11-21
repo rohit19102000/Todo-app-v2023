8ff36eaede5f386885b899d3108ec5c4d79fc371
rohit <rohitkedar2000@gmail.com>
2023-11-21 16:00:29 +0530

    completed design-improvements

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -14,25 +14,31 @@ function TodoList() {
 
   return (
     <div className='todoList'>
+         {filteredTodoList.length > 0 ? (
+        <h3 className={todo.category === 'Done' ? 'status-done' : 'status-pending'}>
+          {todo.category === 'Done' ? 'Completed Tasks' : 'Pending Tasks'}
+        </h3>
+      ) : null}
       {filteredTodoList.map((item) => (
+        
         <div key={item.id} className='todoCard' 
         style={{ border: `2px solid ${item.status ? '#5de800' : '#ffbf00'}` }}>
-          <strong>{item.text}</strong>
+          <strong className={item.status ?"status-done":"status-pending" }>{item.text}</strong>
           <p style={{ fontSize: '.5rem' }}>{item.status === true ? 'done' : 'pending'}</p>
           <div className='details'>
             <p>{item.time}</p>
             <p>{item.date}</p>
             <h6></h6>
-            <button style={{ fontSize: '1.5rem', padding: '5px' }} onClick={() => editTodo(item.id)}>
+            <button className='edit' style={{ fontSize: '1.5rem', padding: '5px' }} onClick={() => editTodo(item.id)}>
               ✍
             </button>
             
-          <button 
+          <button  className={item.status ? "btnA":"btnB"}
                   onClick={() => toggleStatus(item.id)}
         >
     {item.status ? 'X' : '✔'}
   </button>
-            <button onClick={() => removeTodo(item.id)}>🗑</button>
+            <button className='delete' onClick={() => removeTodo(item.id)}>🗑</button>
           </div>
         </div>
       ))}
